a87a7fa7f62833f5178c0ffae06257a8a1fe6e9a
Favier Guillaume <guillaume.favier.dev@gmail.com>
2024-01-16 15:51:35 +0100

    la ultima

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const dev = true
 const https = require("https");
 const http = require("http");
 const fs = require("fs");
@@ -16,28 +15,17 @@ const app = express();
 app.use('/', express.static('static'))
 require('./routes')(app);
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+log(1, "Starting server on port 443")
+https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'ssl/server.key'), 'utf-8'),
+    cert: fs.readFileSync(path.join(__dirname, 'ssl/server.cert'), 'utf-8'),
+    "ca": fs.readFileSync(path.join(__dirname, 'ssl/ca.cert'), 'utf-8')
+}, app).listen(443, () => {
+    log(1,"Server started on port 443")
 });
-
-if (dev) {
-    // listen for requests
-    log(1,"Starting server on port 80 because it's in dev mode")
-    http.createServer(app).listen(80, () => {
-        log(1, "Server started on port 80")
-    });
-}else{
-    log(1, "Starting server on port 443 because it's in production mode")
-    https.createServer({
-        key: fs.readFileSync(path.join(__dirname, 'ssl/server.key')),
-        cert: fs.readFileSync(path.join(__dirname, 'ssl/server.cert'))
-    }, app).listen(443, () => {
-        log(1,"Server started on port 443")
-    });
-    http.createServer((req,res) => {
-        res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-        res.end();
-    }).listen(80, () => {
-        log(1,"Server started on port 80")
-    });
-}
+http.createServer((req,res) => {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+}).listen(80, () => {
+    log(1,"Server started on port 80")
+});
