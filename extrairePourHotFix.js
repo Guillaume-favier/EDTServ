70be480870f7c3d602ed00630f1bcb0f9f11fc33
Favier Guillaume <guillaume.favier.dev@gmail.com>
2024-02-08 03:02:32 +0100

    pour Emma

--- extrairePourHotFix.js
+++ extrairePourHotFix.js
@@ -1,13 +1,16 @@
-const {allEdt} = require("./edt.js")
-const stringify = require("json-stringify-pretty-compact")
-s = 19
-j = 4
-res = allEdt[s]
-let term = []
-res.forEach((element,index) => {
-    if (index+1 != 10)  return
-    term.push([(index + 1).toString(), element[0][j], element[1]])
+const { allEdt } = require("./edt.js");
+const stringify = require("json-stringify-pretty-compact");
+s = 21;
+j = 3;
+res = allEdt[s];
+let term = [];
+res.forEach((element, index) => {
+    if (index + 1 != 4) return;
+    term.push([(index + 1).toString(), element[0][j], element[1]]);
 });
-let temp = {}
-temp[s+"/"+j] = term
-require("fs").writeFileSync("out.json", stringify(temp, { maxLength: 80, indent: 4 }))
+let temp = {};
+temp[s + "/" + j] = term;
+require("fs").writeFileSync(
+    "out.json",
+    stringify(temp, { maxLength: 80, indent: 4 }),
+);
