0c71011457e0507b5461d0982cb11407521928da
Favier Guillaume <guillaume.favier.dev@gmail.com>
2024-05-30 19:34:45 +0200

    🪲 Je crois que c'est le dernier

--- anayse stats/connexionHeatmap.js
+++ anayse stats/connexionHeatmap.js
@@ -2,7 +2,7 @@ const graphToHeatmap = (tograph) => {
     let heatmap = []
     let s2 = true
     tograph.forEach(element => { // chaque élément de la liste représente un jour composé d'une liste avec toutes les heures
-        jour = (new Date(element.time)).setHours(0, 0, 0, 0)
+        jour = new Date((new Date(element.time)).setHours(0, 0, 0, 0)).toISOString()
         if (s2 || heatmap[heatmap.length - 1].jour != jour) {
             s2 = false
             let temp = {
@@ -11,7 +11,7 @@ const graphToHeatmap = (tograph) => {
                 temp[i+"h"] = 0
             }
 
-            temp["jour"] = (new Date(jour)).toISOString()
+            temp["jour"] = jour
             heatmap.push(temp)
         }
         heatmap[heatmap.length - 1][(new Date(element.time)).getHours()+"h"] += element.conn
