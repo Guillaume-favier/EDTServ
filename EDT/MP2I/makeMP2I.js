92f0ef43f9797da20d86ef35a7199f7044497151
Favier Guillaume <guillaume.favier.dev@gmail.com>
2024-08-17 16:08:44 -0400

    🧹 nétoyage de code

--- EDT/MP2I/makeMP2I.js
+++ EDT/MP2I/makeMP2I.js
@@ -51,7 +51,7 @@ const getNumJours = (semaine) => { // donne les dates des jours scolaire pour un
 };
 
 const makeEDT = (groupe, semaine, info = null) => {
-    if (semaine >= 1 && semaine <= 19) return [makeEDT1(groupe, semaine), ""];
+    if (semaine >= 1 && semaine <= 19) return [makeEDT1(groupe, semaine), "Attention béta ! Les cours ne sont très probablement pas corrects."];
     if (semaine >= 20 && semaine <= 35) return makeEDT2(groupe, semaine, info);
     return null;
 };
