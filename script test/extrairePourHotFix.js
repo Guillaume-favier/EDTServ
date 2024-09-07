db1af1b45d23e48a75a86d6220728e51ff3779bd
Guillaume-favier <guillaume.favier.dev@gmail.com>
2024-09-07 18:17:04 +0200

    🔥 modification des hotfixs pour le nouveau format + adaptation de la semaine prochaine

--- script test/extrairePourHotFix.js
+++ script test/extrairePourHotFix.js
@@ -1,11 +1,14 @@
-const { allEdt } = require("../edt.js");
+const { CallEdt } = require("../EDT/edt.js");
 const stringify = require("json-stringify-pretty-compact");
-s = 29;
-j = 2;
-res = allEdt[s];
+s = 2;
+j = 4; // lundi = 0 vendredi = 4
+classe = "MPI";
+res = CallEdt[classe][s];
+console.log(res)
 let term = [];
-res.forEach((element, index) => {
-    term.push([(index + 1).toString(), element[0][j], element[1]]);
+Object.keys(res).forEach((element, index) => {
+    const e = res[element]
+    term.push([element, e[0][j], e[1]]);
 });
 let temp = {};
 temp[s + "/" + j] = term;
